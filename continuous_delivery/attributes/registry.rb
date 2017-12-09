a191514e519c416639df65cc96acf89afaa0256a
Aleix Penella <aleix.penella@gmail.com>
2017-12-09 23:56:44 +0100

    Registry and registry-ui deployed using continuous_delivery_service resource

--- continuous_delivery/attributes/registry.rb
+++ continuous_delivery/attributes/registry.rb
@@ -5,9 +5,21 @@
 # 2017 Aleix Penella
 #
 
-# service name
-default['registry']['service'] = 'registry'
+#
+# attributes for deployment node
+default['registry']['deploy']['clear'] = false
 
+#
+# systemd service definition
+default['registry']['service'] = 'registry'
+default['registry']['systemd'] = {
+	'name': node['registry']['service'],
+	'description': 'Service for private docker registry',
+	'requires': node['docker']['service'],
+	'after': node['docker']['service']
+}
+
+#
 # registry configuration
 default['registry']['config']['host'] = '0.0.0.0'
 default['registry']['config']['port'] = 5000
@@ -15,6 +27,7 @@ default['registry']['config']['protocol'] = 'http'
 default['registry']['config']['addr'] = "#{node['registry']['config']['host']}:#{node['registry']['config']['port']}"
 default['registry']['config']['storage'] = "/var/lib/registry"
 
+#
 # docker images
 default['registry']['docker']['image'] = {
 	'name': 'registry',
@@ -22,9 +35,10 @@ default['registry']['docker']['image'] = {
 	'action': 'pull_if_missing'
 }
 
+#
 # docker containers
 default['registry']['docker']['container'] = {
-	'name': 'registry',
+	'name': "#{node['registry']['service']}",
 	'repo': "#{node['registry']['docker']['image'].name}",
 	'tag': "#{node['registry']['docker']['image'].tag}",
 	'port': "5000:#{node['registry']['config']['port']}",
--- continuous_delivery/recipes/registry.rb
+++ continuous_delivery/recipes/registry.rb
@@ -5,55 +5,21 @@
 # 2017 Aleix Penella
 #
 
-# clear systemd service
 #
-# systemctl stop [servicename]
-# systemctl disable [servicename]
-# rm /etc/systemd/system/[servicename]
-# rm /etc/systemd/system/[servicename] symlinks that might be related
-# systemctl daemon-reload
-# systemctl reset-failed
-
-#
-# pull docker image
-docker_image node['registry']['docker']['image'].name do
-	tag node['registry']['docker']['image'].tag
-	action node['registry']['docker']['image'].action
+# clear service
+if node['registry']['deploy']['clear'] then
+	continuous_delivery_service "Clear #{node['registry']['service']}" do
+		image node['registry']['docker']['image']
+		container node['registry']['docker']['container']
+		systemd_service node['registry']['systemd']
+		action :clear
+	end
 end
 
 #
-# run container
-docker_container node['registry']['docker']['container'].name do
-	repo 	node['registry']['docker']['container'].repo
-	tag 	node['registry']['docker']['container'].tag
-	port  	node['registry']['docker']['container'].port
-	if node['registry']['docker']['container'].has_key?('volumes')	then volumes node['registry']['docker']['container'].volumes end
-	if node['registry']['docker']['container'].has_key?('env') 	then env node['registry']['docker']['container'].env end
-	action 	node['registry']['docker']['container'].action
-end
-
-#
-# define systemd service
-systemd_unit "#{node['registry']['service']}.service" do
-	content <<-EOU.gsub(/^\s+/, '')
-		[Unit]
-		Description=Docker registry service
-		Requires=docker.service
-		After=docker.service
-		[Service]
-		TimeoutStartSec=0
-		ExecStart=/usr/bin/docker start -a registry
-		ExecStop=/usr/bin/docker stop registry
-		Restart=on-failure
-
-		[Install]
-		WantedBy=multi-user.target
-	EOU
-
-	action [:create, :enable] 	
-end
-#
-# registry service
-service "#{node['registry']['service']}" do
-	action :restart
+# deploy service
+continuous_delivery_service node['registry']['service'] do
+	image node['registry']['docker']['image']
+	container node['registry']['docker']['container']
+	systemd_service node['registry']['systemd']
 end
